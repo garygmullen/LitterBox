06ae7243f477dc0031a79bbc2d83afe2d6917c60
Gary Mullen <garygmullen@gmail.com>
2014-12-25 10:22:01 -0800

    Moved messages into dedicated print function.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+static int printMesg(
+   void
+   )
+{
+    printf ( "A silly mesg: Hello World !\n" );
+    printf ( "Turn off syntax expansion." );
+    return 0;
+}
+
 int main( 
    int argc,
    char **argv
    )
 {
-    printf ( "A silly mesg: Hello World !\n" );
-    printf ( "Turn off syntax expansion." );
+    (void) printMesg();
     return 0;
 }
